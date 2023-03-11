070ac135f4a920c8f9ca94e12e473e0af8dc4405
devhouda <113182196+devhouda@users.noreply.github.com>
2023-03-11 17:55:17 +0100

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,9 @@
+#Day 1 - Working with Variables in Python to Manage Data
+
+#Concepts Practised: printing, commenting, debugging, string manipulation and variables
+
+#Band Name Generator:
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grew up in?\n")
+pet = input("What is the name of your favorite pet?\n")
+print("Your band name could be " + city + " " + pet)
