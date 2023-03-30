ed44e7acd92a7ff75e625c36b45355f2dfcdccce
devhouda <113182196+devhouda@users.noreply.github.com>
2023-03-30 23:56:50 +0100

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,22 @@
+#Day 6 - Python Functions & Karel
+
+#Concepts Practised: functions, code blocks and while loops
+
+#Escaping the Maze:
+def turn_right():    
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
